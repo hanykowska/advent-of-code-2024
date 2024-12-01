da51b3bc227001d73f816e5f9fba0bbad44d574d
hanykowska <h.nykowska@gmail.com>
2024-12-01 21:05:31 +0100

    day_1 done

---
+++ code/day_1.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+input_name = "input_1.csv"
+input_path = "inputs/" + input_name
+
+input = pd.read_csv(os.path.abspath(input_path),
+                    sep='\s+',
+                    header=None)  
+
+list_1 = list(input[0])
+list_2 = list(input[1])
+
+list_1 = sorted(list_1)
+list_2 = sorted(list_2)
+
+distance = [None for x in list_1]
+similarity_factor = [None for x in list_1]
+similarity_score = [None for x in list_1]
+
+for i in range(len(list_1)):
+    distance[i] = abs(list_1[i] - list_2[i])
+    similarity_factor[i] = list_2.count(list_1[i])
+    similarity_score[i] = list_1[i] * similarity_factor[i]
+
+
+
+print("The distance is ", sum(distance))
+print("The similarity score is ", sum(similarity_score))
