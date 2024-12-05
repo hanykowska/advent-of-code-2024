8e6bf7742fabf624010f8944eade967fb9e6d967
hanykowska <h.nykowska@gmail.com>
2024-12-05 21:09:28 +0100

    day 5 part 1 complete

--- code/day_5.py
+++ code/day_5.py
@@ -1,38 +1,49 @@
+import os
 
-input_data = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""
+input_name = "input_5.txt"
+input_path = "inputs/" + input_name
+# in macOS or pycharm, ".." is needed to get out of "code" directory,
+# in Windows or VSCode, the starting point is the repository directory
+
+file = open(os.path.abspath(input_path), "r")
+input_data = file.read()
+file.close()
+
+# input_data = """47|53
+# 97|13
+# 97|61
+# 97|47
+# 75|29
+# 61|13
+# 75|53
+# 29|13
+# 97|29
+# 53|29
+# 61|53
+# 97|53
+# 61|29
+# 47|13
+# 75|47
+# 97|75
+# 47|61
+# 75|61
+# 47|29
+# 75|13
+# 53|13
+
+# 75,47,61,53,29
+# 97,61,53,29,13
+# 75,29,13
+# 75,97,47,61,53
+# 61,13,29
+# 97,13,75,29,47"""
 
 input_data = input_data.split("\n\n")
 print(len(input_data))
 
-rules = input_data[0].split("\n")
-updates = input_data[1].split("\n")
+rules = input_data[0].splitlines()
+updates = input_data[1].splitlines()
 
 rules = sorted(rules)
 rules = [rule.split("|") for rule in rules]
