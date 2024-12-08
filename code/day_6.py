9108d7fc3ca619a6473958c0c9521256f51c77bb
hanykowska <h.nykowska@gmail.com>
2024-12-08 21:34:53 +0100

    some progress with movement

--- code/day_6.py
+++ code/day_6.py
@@ -1,8 +1,7 @@
 import re
 import pandas as pd
 
-input_data = """
-....#.....
+input_data = """....#.....
 .........#
 ..........
 ..#.......
@@ -28,41 +27,45 @@ def go_up(map_list, position):
     x,y = position
     count = 0
     while y-1 >= 0 and map_list[y-1][x] != "#":
+        map_list[y] = map_list[y][:x] +  "X" + map_list[y][x+1:]
         count += 1
         y -= 1
-    if y-1==0:
-        return count, None, None
-    return count, [x,y], ">"
+    if y==0:
+        return count, None, None, map_list
+    return count, (x,y), ">", map_list
 
 def go_down(map_list, position):
     x, y = position
     count = 0
-    while y+1 >= len(map_list) and map_list[y+1][x] != "#":
+    while y+1 <= len(map_list) and map_list[y+1][x] != "#":
+        map_list[y] = map_list[y][:x] +  "X" + map_list[y][x+1:]
         count += 1
         y += 1
     if y+1 == len(map_list):
-        return count, None, None
-    return count, [x, y], "<"
+        return count, None, None, map_list
+    return count, (x,y), "<", map_list
 
 def go_right(map_list, position):
     x, y = position
     count = 0
-    while x+1 >= len(map_list[0]) and map_list[y][x+1] != "#":
+    while x+1 <= len(map_list[0]) and map_list[y][x+1] != "#":
+        map_list[y] = map_list[y][:x] +  "X" + map_list[y][x+1:]
         count += 1
         x += 1
-    if x+1 == len(map_list[0]):
-        return count, None, None
-    return count, [x, y], "v"
+    if x == len(map_list[0]):
+        return count, None, None, map_list
+    return count, (x,y), "v", map_list
 
 def go_left(map_list, position):
     x, y = position
     count = 0
     while x-1 >= 0 and map_list[y][x-1] != "#":
+        map_list[y] = map_list[y][:x] +  "X" + map_list[y][x+1:]
         count += 1
         x -= 1
     if x-1 == 0:
-        return count, None, None
-    return count, [x, y], "^"
+        return count, None, None, map_list
+    return count, (x,y), "^", map_list
 
 
 def go_to_next_obstacle(map_list, position, direction):
@@ -79,15 +82,19 @@ def go_to_next_obstacle(map_list, position, direction):
         return go_left(map_list, position)
 
 cnt = 1
-pos , dirn = position, direction
-for i in range(4):
-    temp_cnt, pos, dirn = go_to_next_obstacle(input_data, pos, dirn)
+pos , dirn , new_map = position, direction, input_data
+while pos is not None:
+    temp_cnt, pos, dirn, new_map = go_to_next_obstacle(new_map, pos, dirn)
     print(temp_cnt, pos, dirn)
     cnt += temp_cnt
+    if pos is None:
+        print("End of map")
+        break
 
 
 print(cnt)
-print(input_data)
+for line in new_map:
+    print(line)
 
 
 
